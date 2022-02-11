9e72f98076170b8a15176db69b833190bee3b2a0
Daniel Miolan <miolan96@gmail.com>
2022-02-10 21:16:41 -0400

    Create project.

---
+++ app/src/main/java/com/example/lab_1_2_danielmiolan_c0817504_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab_1_2_danielmiolan_c0817504_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
